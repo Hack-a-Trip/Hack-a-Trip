55b7b762977486f975cc77406299c0609e4689c4
p4ddl3 <simon.daget@gmail.com>
2013-11-29 11:09:33 +0100

    fixmail2

--- Hack-a-Trip/src/net/tncy/hackatrip/InvitationMail.java
+++ Hack-a-Trip/src/net/tncy/hackatrip/InvitationMail.java
@@ -15,32 +15,32 @@ import com.google.appengine.api.users.UserServiceFactory;
 import net.tncy.entity.Travel;
 
 public class InvitationMail {
-	public static void send(String mail, Travel travel) {
+        public static void send(String mail, Travel travel) {
 
-		UserService userService = UserServiceFactory.getUserService();
-		User user = userService.getCurrentUser();
-		if(user != null){
-			Properties props = new Properties();
-			Session session = Session.getDefaultInstance(props, null);
-			
-			String msgBody = "Bonjour,\n" +
-					"\n" +
-					user.getNickname()+" a le plaisir de vous inviter au voyage '"+travel.getName()+"' !" +
-							"Vous pouvez participer à la préparation de cet évenement ici : http://hackatrip.appspot.com/DisplayTravel?id="+travel.getId() + " \n" +
-							"\n" +
-							"A bientôt, sur Hack a trip!";
-			try{
-				Message msg = new MimeMessage(session);
-				msg.setFrom(new InternetAddress("noreply@hackatrip.appspot.com", "Invitation Hack a Trip"));
-				msg.addRecipient(Message.RecipientType.TO,
-						new InternetAddress(mail, "Invité"));
-				msg.setSubject(user.getNickname()+" vous invite à voyager, depuis le site Hackatrip.appspot.com");
-				msg.setText(msgBody);
-				Transport.send(msg);
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-		}
-	}
+                UserService userService = UserServiceFactory.getUserService();
+                User user = userService.getCurrentUser();
+                if(user != null){
+                        Properties props = new Properties();
+                        Session session = Session.getDefaultInstance(props, null);
+                        
+                        String msgBody = "Bonjour,\n" +
+                                        "\n" +
+                                        user.getNickname()+" a le plaisir de vous inviter au voyage '"+travel.getName()+"' !" +
+                                                        "Vous pouvez participer à la préparation de cet évenement ici : http://hackatrip.appspot.com/DisplayTravel?id="+travel.getId() + " \n" +
+                                                        "\n" +
+                                                        "A bientôt, sur Hack a trip!";
+                        try{
+                                Message msg = new MimeMessage(session);
+                                msg.setFrom(new InternetAddress(user.getEmail(), "Invitation Hack a Trip"));
+                                msg.addRecipient(Message.RecipientType.TO,
+                                                new InternetAddress(mail, "Invité"));
+                                msg.setSubject(user.getNickname()+" vous invite à voyager, depuis le site Hackatrip.appspot.com");
+                                msg.setText(msgBody);
+                                Transport.send(msg);
+                        }catch(Exception e){
+                                e.printStackTrace();
+                        }
+                }
+        }
 
 }
